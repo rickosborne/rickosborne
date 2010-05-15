4860cba0a72ed3fab0de42a633040328b61e8bfa
Rick Osborne <ricko@rickosborne.org>
2010-05-15 00:40:28 -0400

    Updated authors and cats to include post list

--- couchdb/wordpress-to-json.sql
+++ couchdb/wordpress-to-json.sql
@@ -3,19 +3,19 @@ SELECT CONCAT(
   '{',
   '"_id": "post-', ID, '", ',
   '"id": ', ID, ', ',
-  '"cats": {', COALESCE((SELECT GROUP_CONCAT(DISTINCT CONCAT('"',t.slug,'": ',tt.term_id) SEPARATOR ', ') FROM wp_term_relationships AS tr INNER JOIN wp_term_taxonomy AS tt ON (tt.taxonomy = 'category') AND (tr.term_taxonomy_id = tt.term_taxonomy_id) INNER JOIN wp_terms AS t ON (t.term_id = tt.term_id) WHERE (tr.object_id = p.ID)), ''), '}, '
-  '"tags": [', COALESCE((SELECT GROUP_CONCAT(DISTINCT CONCAT('"',t.slug,'"') SEPARATOR ', ') FROM wp_term_relationships AS tr INNER JOIN wp_term_taxonomy AS tt ON (tt.taxonomy = 'post_tag') AND (tr.term_taxonomy_id = tt.term_taxonomy_id) INNER JOIN wp_terms AS t ON (t.term_id = tt.term_id) WHERE (tr.object_id = p.ID)), ''), '], '
+  '"cats": {', COALESCE((SELECT GROUP_CONCAT(DISTINCT CONCAT('"',t.slug,'": ',tt.term_id) SEPARATOR ', ') FROM wp_term_relationships AS tr INNER JOIN wp_term_taxonomy AS tt ON (tt.taxonomy = 'category') AND (tr.term_taxonomy_id = tt.term_taxonomy_id) INNER JOIN wp_terms AS t ON (t.term_id = tt.term_id) WHERE (tr.object_id = p.ID)), ''), '}, ',
+  '"tags": [', COALESCE((SELECT GROUP_CONCAT(DISTINCT CONCAT('"',t.slug,'"') SEPARATOR ', ') FROM wp_term_relationships AS tr INNER JOIN wp_term_taxonomy AS tt ON (tt.taxonomy = 'post_tag') AND (tr.term_taxonomy_id = tt.term_taxonomy_id) INNER JOIN wp_terms AS t ON (t.term_id = tt.term_id) WHERE (tr.object_id = p.ID)), ''), '], ',
   '"author": ', post_author, ', ',
   '"posted": "', post_date, '", ',
   '"posted_gmt": "', post_date_gmt, '", ',
   '"content": "', REPLACE(REPLACE(REPLACE(REPLACE(post_content, '\\', '\\\\'), '"', '\\"'), '=\\"/', '=\\"http://rickosborne.org/'), '\r\n', '\\n'), '", ',
   '"title": "', REPLACE(REPLACE(post_title, '\\', '\\\\'), '"', '\\"'), '", ',
-  '"STATUS": "', post_status, '", ',
-  '"NAME": "', post_name, '", ',
+  '"status": "', post_status, '", ',
+  '"name": "', post_name, '", ',
   '"modified": "', post_modified, '", ',
   '"modified_gmt": "', post_modified_gmt, '", ',
   '"guid": "', guid, '", ',
-  '"TYPE": "', post_type, '"',
+  '"type": "', post_type, '"',
   '}') AS json
 FROM wp_posts AS p
 WHERE (post_parent = 0)
@@ -28,13 +28,14 @@ LIMIT 10;
 SELECT CONCAT('{'
   '"_id": "author-', ID, '", ',
   '"id": ', ID, ', '
-  '"NAME": "', display_name, '", ',
+  '"name": "', display_name, '", ',
+  '"posts": {', COALESCE((SELECT GROUP_CONCAT(DISTINCT CONCAT('"',p.post_name,'": ',p.id) SEPARATOR ', ') FROM wp_posts AS p WHERE (p.post_author = u.ID) AND (p.id > 1126) AND (p.post_parent = 0)), ''), '}, ',
   CASE WHEN user_url <> '' THEN CONCAT('"url": "', user_url, '", ') ELSE '' END,
   '"gravatar_url": "http://www.gravatar.com/avatar/', MD5(LOWER(TRIM(user_email))), '", ',
   '"registered": "', user_registered, '", ',
-  '"TYPE": "author"',
+  '"type": "author"',
   '}') AS json
-FROM wp_users
+FROM wp_users AS u
 ORDER BY ID
 LIMIT 3;
 
@@ -42,9 +43,11 @@ LIMIT 3;
 SELECT CONCAT('{',
   '"_id": "cat-', cat_ID, '", ',
   '"id": ', cat_ID, ', ',
-  '"NAME": "', cat_name, '", ',
+  '"name": "', cat_name, '", ',
+  '"posts": {', COALESCE((SELECT GROUP_CONCAT(DISTINCT CONCAT('"',p.post_name,'": ',p.id) SEPARATOR ', ') FROM wp_term_relationships AS tr INNER JOIN wp_term_taxonomy AS tt ON (tt.taxonomy = 'category') AND (tr.term_taxonomy_id = tt.term_taxonomy_id) INNER JOIN wp_posts AS p ON (p.ID = tr.object_id) INNER JOIN wp_terms AS t ON (tt.term_id = t.term_id) WHERE (t.slug = c.category_nicename) AND (p.id > 1126)), ''), '}, ',
   CASE WHEN category_parent > 0 THEN CONCAT('"parent_id": ', category_parent, ', "parent": "cat-', category_parent, '", ') ELSE '' END,
   '"nicename": "', category_nicename, '",',
-  '"TYPE": "category"'
+  '"type": "category"'
   '}') AS json
-FROM wp_categories
+FROM wp_categories AS c
+
