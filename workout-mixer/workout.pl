8f51c8f6acd6a53ccf73b4c15db15b1b1f99badb
Rick Osborne <ricko@rickosborne.org>
2012-10-14 00:54:04 -0400

    Generates MP4 files that look about right

--- workout-mixer/workout.pl
+++ workout-mixer/workout.pl
@@ -7,28 +7,90 @@ use strict;
 
 my $libraryPath = $ENV{'HOME'} . '/Music/iTunes/iTunes Music Library.xml';
 my $mix = '30'; # '5@130-150:140-150;20@150-165:160;5@130-150:150-140'
-my $outputPath = 'workout.mp3';
+my $tempDir = '__workout_temp__';
+my $outputTitle = 'workout';
 my $verbose = '';
 my $library;
-my $songFilter = '';
+my $acceptFilter = '';
+my $rejectFilter = '';
 my $cutoff = 15;
+my $bitrate = 128;
+my $artist = '';
+my $year = (localtime())[5] + 1900;
 
 GetOptions(
     'library=s' => \$libraryPath,
     'mix=s'     => \$mix,
-    'out=s'     => \$outputPath,
-    'filter=s'  => \$songFilter,
+    'title=s'   => \$outputTitle,
+    'accept=s'  => \$acceptFilter,
+    'reject=s'  => \$rejectFilter,
+    'artist=s'  => \$artist,
+    'bitrate=i' => \$bitrate,
     'cutoff=i'  => \$cutoff,  
     'verbose'   => \$verbose
 );
 
 my @sequence = sequenceFromMix($mix);
 # print Dumper(\@sequence);
-my $songCache = cacheSongsForSequence(\@sequence, $songFilter);
+my $songCache = cacheSongsForSequence(\@sequence);
 # print Dumper($songCache);
 my $playlist = buildPlaylist(\@sequence, $songCache);
-print Dumper($playlist);
+# print Dumper($playlist);
+# unlink($tempDir) if(-d $tempDir);
+# mkdir($tempDir);
+my $songN = 0;
+my $wavFiles = '';
+open(CHAP,">$outputTitle.chapters.txt");
+open(FAAC,"| faac -b $bitrate -o '$outputTitle.m4a' --title '$outputTitle' --artist '$artist' --year $year -s -");
+select(FAAC); $|=1; select(STDOUT);
+my $place = 0;
+my $type = 'wav';
+foreach my $song (@{$playlist}) {
+    $songN++;
+    my $artist = $song->{'artist'};
+    my $title = $song->{'title'};
+    my $album = $song->{'album'};
+    my $tempo = $song->{'tempo'};
+    my $invTempo = 1.0 / $tempo;
+    my $path = $song->{'path'};
+    my $pace = $song->{'pace'};
+    my $cut = $song->{'cut'};
+    my $ms = $song->{'ms'};
+    my $startMS = int(($ms - $cut) / 2);
+    my $finishMS = $ms - $startMS;
+    my $startTime = formatMS($startMS);
+    my $finishTime = formatMS($finishMS);
+    debug("Song: $title $pace, $tempo, " . formatMS($cut) . " > " . formatMS($cut / $tempo));
+    # my $file = "'$tempDir/$songN.wav'";
+    # $wavFiles .= " $file";
+    my $index = formatMS($place);
+    # print qq!sox --norm "$path" -t wav -c 2 -r 44100 -b 16 - trim =$startTime =$finishTime | sox -t wav - -t $type -c 2 -r 44100 -b 16 - tempo -m $tempo |!; # tempo -m $tempo
+    open(SOX, qq!sox -q -V0 --norm "$path" -t $type -c 2 -r 44100 -b 16 - trim =$startTime =$finishTime tempo -m $tempo |!); # tempo -m $tempo
+    select(SOX); $|=1; select(STDOUT);
+    while(<SOX>) { print FAAC; }
+    close(SOX);
+    print CHAP "CHAPTER$songN=$index\nCHAPTER${songN}NAME=$artist / $album / $title\n";
+    $place += int($cut / $tempo);
+    $type = 'raw';
+}
+close(FAAC);
+close(CHAP);
+# system("play -t wav $wavFiles - | faac -b $bitrate -o '$tempDir/$outputTitle.m4a' -");
+system("mp4chaps -i '$outputTitle.m4a'");
+# for (my $i = 0; $i <= $songN; $i++) {
+#    unlink("$tempDir/$i.wav");
+#}
+unlink("$outputTitle.chapters.txt");
+#unlink($tempDir);
 exit(0);
+
+sub formatMS {
+    my ($t) = @_;
+    my $ms = $t % 1000; $t = int($t / 1000);
+    my $s = $t % 60; $t = int($t / 60);
+    my $m = $t % 60; $t = int($t / 60);
+    return $t . ":" . substr("00$m:",-3,3) . substr("00$s.",-3,3) . substr("000$ms",-3,3);
+}
 
 sub buildPlaylist {
     my ($sequence, $songs) = @_;
@@ -45,7 +107,7 @@ sub buildPlaylist {
         while ($ms < $msTarget) {
             my $song = nextSongForPace($minPace, $maxPace, $songs);
             $song->{'tempo'} = ($startPace eq '*') ? 1 : ($startPace + (($ms / $msTarget) * $paceRange)) / $song->{'pace'};
-            $ms += $song->{'ms'} * $song->{'tempo'};
+            $ms += $song->{'ms'} / $song->{'tempo'};
             push @parts, $song;
         }
         my $scale = $msTarget / $ms;
@@ -67,13 +129,13 @@ sub nextSongForPace {
 } # nextSongForPace
 
 sub cacheSongsForSequence {
-    my ($sequence, $songFilter) = @_;
+    my ($sequence) = @_;
     my %cache;
     foreach my $segment (@$sequence) {
         my $key = $segment->{'inMin'} . '-' . $segment->{'inMax'};
         unless (defined($cache{$key})) {
             debug("Songs for pace: $key");
-            my $songs = getSongsForPaceRange($segment->{'inMin'}, $segment->{'inMax'}, $songFilter);
+            my $songs = getSongsForPaceRange($segment->{'inMin'}, $segment->{'inMax'});
             $cache{$key} = $songs;
         }
     }
@@ -105,17 +167,28 @@ sub shuffle { # via http://www.perlmonks.org/?node_id=1869
 }
 
 sub filterSong {
-    my ($song, $filter) = @_;
-    return (($song->album() =~ /$filter/i)
-     || ($song->name() =~ /$filter/i)
-     || ($song->artist() =~ /$filter/i));
+    my ($song) = @_;
+    if (($acceptFilter eq '') && ($rejectFilter eq '')) { return 1; }
+    if ($rejectFilter ne '') {
+        if (($song->album() =~ /$rejectFilter/i)
+         || ($song->name() =~ /$rejectFilter/i)
+         || ($song->artist() =~ /$rejectFilter/i)) { return 0; }
+    }
+    if ($acceptFilter ne '') {
+        if (($song->album() =~ /$acceptFilter/i)
+         || ($song->name() =~ /$acceptFilter/i)
+         || ($song->artist() =~ /$acceptFilter/i)) { return 1; }
+        return 0;
+    }
+    return 1;
 }
 
 sub getSongsForPaceRange {
-    my ($minPace, $maxPace, $songFilter) = @_;
+    my ($minPace, $maxPace) = @_;
     my $library = getLibrary();
     my %items = $library->items();
     my @songs = ();
+    my %dupes = ();
     while (my ($artist, $artistSongs) = each %items) {
         while (my ($songName, $artistSongItems) = each %$artistSongs) {
             foreach my $item (@$artistSongItems) {
@@ -126,12 +199,17 @@ sub getSongsForPaceRange {
                 my $ms = int($item->totalTime());
                 my $path = urldecode($item->location());
                 $path =~ s!^file\://localhost!!;
-                next unless(-f $path);
-                next unless(($songFilter eq '') || filterSong($item, $songFilter));
+                next unless((substr($path,-4,4) eq '.mp3') && (-f $path));
+                next unless(filterSong($item));
                 next unless($cutoff * 60000 >= $ms);
+                my $artist = $item->artist();
+                my $title = $item->name();
+                next if(defined($dupes{$artist . ' / ' . $title}));
+                $dupes{$artist . ' / ' . $title} = 1;
                 my %song = (
-                    'artist' => $item->artist(),
-                    'title'  => $item->name(),
+                    'artist' => $artist,
+                    'album'  => $item->album(),
+                    'title'  => $title,
                     'path'   => $path,
                     'pace'   => $pace,
                     'ms'     => $ms
